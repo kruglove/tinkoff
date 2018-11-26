e1e40e08f28cc25ff7d302dc2001cd44f3c96f0b
kruglove <sl2857@mail.ru>
2018-11-26 23:30:10 +0700

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -2,5 +2,17 @@ import requests
 
 
 def test_script(http_client):
-    response = http_client.get_sub(request_id='1', system_code='2')
+    response = http_client.get_sub(request_id='1', system_code='2', 'e.kruglova')
     assert response.status_code == 200
+
+def test_first(http_client):
+    response = http_client.post_sub('1', '2', 'TCS_SPBXM', 'TCS', 'equity', 20, 'e.kruglova')
+    assert response.status_code == 201
+
+def test_delete(http_client):
+    response = http_client.del_sub( '1', '1', '2', 'e.kruglova')
+    assert response.status_code == 200
+
+def test_get(http_client, id):
+    response = http_client.get_sub(request_id='1', system_code='2','e.kruglova')
+    assert response.body.get('id') == id
