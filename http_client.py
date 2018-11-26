de3a7d1d519e15e652fae93c4d0bb0505282b907
kruglove <sl2857@mail.ru>
2018-11-26 23:29:28 +0700

    Update http_client.py

--- http_client.py
+++ http_client.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 
 class HttpClient(object):
 
@@ -7,13 +7,22 @@ class HttpClient(object):
         self.base_url = 'https://fintech-trading-qa.tinkoff.ru/v1/md'
         self.headers = {'Authorization': 'Basic ZmludGVjaDoxcTJ3M2Uh'}
 
-    def get_sub(self,request_id,system_code):
-        url = self.base_url + '/contacts/e.kruglova/subscriptions?request_id=='+request_id+'&system_code='+system_code
+    def get_sub(self,request_id,system_code, siebel_id):
+        url = self.base_url + '/contacts/' + siebel_id+'/subscriptions?request_id=='+request_id+'&system_code='+system_code
         response = requests.get(url, headers=self.headers)
         return response
 
-    def del_sub(self, request_id, system_code, sub_id):
-        url = self.base_url + '/contacts/e.kruglova/subscriptions/'+sub_id+'?request_id==' + request_id + '&system_code=' + system_code
+    def del_sub(self, request_id, system_code, subscription_id, siebel_id ):
+        url = self.base_url + '/contacts/' + siebel_id+ '/subscriptions/'+subscription_id +'?request_id==' + request_id + '&system_code=' + system_code
         response = requests.delete(url, headers=self.headers)
         return response
+
+    def post_sub(self, request_id, system_code, instrument_id, sec_name, sec_type, price_alert, siebel_id):
+        url = self.base_url + "contacts/" + siebel_id + "/subscriptions?request_id=" + request_id + "&system_code=" + system_code
+        head = {'Authorization': 'Basic ZmludGVjaDoxcTJ3M2Uh', 'Content-type': 'application/json'}
+        data = {"body": [{"instrument_id": instrument_id, "sec_name": sec_name, "sec_type": sec_type,
+                                                                                        "price_alert": price_alert}]}
+        return requests.post(url, json=json.dumps(data), headers=head)
+
+
 
